d3ff6ab28b7ee3448b946b53ac1071663f702c11
katie026 <kmjbcw@gmail.com>
2024-02-13 01:58:38 -0600

    Created initial Xcode project.

---
+++ MUSitesMobile/MUSitesMobile/MUSitesMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUSitesMobileApp.swift
+//  MUSitesMobile
+//
+//  Created by J Kim on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MUSitesMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
