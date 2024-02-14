e9e83013ddf72bf422e7bbd7359e83fc20161e93
katie026 <kmjbcw@gmail.com>
2024-02-14 03:33:57 -0600

    Created AuthenticationView, AuthenticationManager, and SignInEmailView. Successfully created a new user in Firebase.

---
+++ MUSitesMobile/MUSitesMobile/Authentication/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  MUSitesMobile
+//
+//  Created by J Kim on 2/14/24.
+//
+
+import Foundation
---
+++ MUSitesMobile/MUSitesMobile/Authentication/SignInEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInEmailView.swift
+//  MUSitesMobile
+//
+//  Created by J Kim on 2/14/24.
+//
+
+import SwiftUI
+
+struct SignInEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInEmailView()
+}
