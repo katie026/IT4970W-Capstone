0b3646f6c1ca6b791223157f88ba0f1fa98118e3
katie026 <kmjbcw@gmail.com>
2024-04-01 03:36:15 -0500

    Manually merged feature/inventory-submission-view branch and main branch.

---
+++ MUSitesMobile/MUSitesMobile/Core/Components/HelperViews/SmallViews.swift
@@ -0,0 +1,60 @@
+//
+//  SmallViews.swift
+//  MUSitesMobile
+//
+//  Created by Katie Jackson on 3/22/24.
+//
+
+import SwiftUI
+
+struct RadioButton: View {
+    let text: String
+    let isSelected: Bool
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            HStack {
+                Image(systemName: isSelected ? "largecircle.fill.circle" : "circle")
+                Text(text)
+            }
+        }
+    }
+}
+
+// https://stackoverflow.com/questions/56760335/how-to-round-specific-corners-of-a-view
+struct RoundedCorners: View {
+    var color: Color = .blue
+    var tl: CGFloat = 0.0
+    var tr: CGFloat = 0.0
+    var bl: CGFloat = 0.0
+    var br: CGFloat = 0.0
+
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+
+                let w = geometry.size.width
+                let h = geometry.size.height
+
+                // Make sure we do not exceed the size of the rectangle
+                let tr = min(min(self.tr, h/2), w/2)
+                let tl = min(min(self.tl, h/2), w/2)
+                let bl = min(min(self.bl, h/2), w/2)
+                let br = min(min(self.br, h/2), w/2)
+
+                path.move(to: CGPoint(x: w / 2.0, y: 0))
+                path.addLine(to: CGPoint(x: w - tr, y: 0))
+                path.addArc(center: CGPoint(x: w - tr, y: tr), radius: tr, startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 0), clockwise: false)
+                path.addLine(to: CGPoint(x: w, y: h - br))
+                path.addArc(center: CGPoint(x: w - br, y: h - br), radius: br, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
+                path.addLine(to: CGPoint(x: bl, y: h))
+                path.addArc(center: CGPoint(x: bl, y: h - bl), radius: bl, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 180), clockwise: false)
+                path.addLine(to: CGPoint(x: 0, y: tl))
+                path.addArc(center: CGPoint(x: tl, y: tl), radius: tl, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
+                path.closeSubpath()
+            }
+            .fill(self.color)
+        }
+    }
+}
---
+++ MUSitesMobile/MUSitesMobile/Firestore/InventoryEntryManager.swift
@@ -0,0 +1,16 @@
+//
+//  InventoryEntryManager.swift
+//  MUSitesMobile
+//
+//  Created by Katie Jackson on 3/22/24.
+//
+
+import Foundation
+
+enum InventoryEntryType {
+    case Check
+    case Fix
+    case Delivery
+    case Use
+    case Move
+}
