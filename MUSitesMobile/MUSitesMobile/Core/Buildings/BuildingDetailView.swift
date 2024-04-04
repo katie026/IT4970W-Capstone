c499bd0a6a442d30c1c911909569efa8bf10a900
cassiebeisheim <40363436+cassiebeisheim@users.noreply.github.com>
2024-04-04 12:48:02 -0500

    Created BuildingDetailView. Redirected Maps pins to view. Added Inventory Sites and Sites lists for each building to the view.

---
+++ MUSitesMobile/MUSitesMobile/Core/Buildings/BuildingDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  BuildingDetailView.swift
+//  MUSitesMobile
+//
+//  Created by Benjamin Beisheim on 4/4/24.
+//
+
+import SwiftUI
+
+struct BuildingDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BuildingDetailView()
+}
