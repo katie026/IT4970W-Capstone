0bedd579828ba0a76396ef5b917ffbd8d6176f04
katie026 <kmjbcw@gmail.com>
2024-04-04 23:24:41 -0500

    Displaying inventory entries using List and a Subview. Added date pickers to load entries between two dates. Need to load proper site and user names.

---
+++ MUSitesMobile/MUSitesMobile/Core/Inventory/InventoryEntries/SupplyEntriesView.swift
@@ -0,0 +1,18 @@
+//
+//  SupplyEntriesView.swift
+//  MUSitesMobile
+//
+//  Created by Katie Jackson on 4/4/24.
+//
+
+import SwiftUI
+
+struct SupplyEntriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SupplyEntriesView()
+}
