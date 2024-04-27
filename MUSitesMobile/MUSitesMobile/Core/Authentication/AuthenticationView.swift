70e2953f492475fcd7f662642d5198e9536cef98
katie026 <kmjbcw@gmail.com>
2024-04-27 01:39:02 -0500

    Added a few assets. Updated accent color. Redesigned AuthenticationView to be closer to wireframe.

--- MUSitesMobile/MUSitesMobile/Core/Authentication/AuthenticationView.swift
+++ MUSitesMobile/MUSitesMobile/Core/Authentication/AuthenticationView.swift
@@ -6,123 +6,167 @@
 //
 
 import SwiftUI
-import GoogleSignIn // can remove this if we use out own Google Button
-import GoogleSignInSwift // can remove this if we use out own Google Button
+import GoogleSignIn // can remove this if we use our own Google Button
+import GoogleSignInSwift // can remove this if we use our own Google Button
 
 struct AuthenticationView: View {
     // create a view model to hold functions
     @StateObject private var viewModel = AuthenticationViewModel()
     // recieve and link to the showSignInView variable that belongs to RootView
     @Binding var showSignInView: Bool
-    //
+    // alerts
     @State private var showInvalidEmailAlert: Bool = false
-    //Non auth
     @State private var showNonAuthenticatedEmailAlert: Bool = false
-    
     @State private var showInvalidLoginAlert: Bool = false
     
     var body: some View {
-        VStack {
-            // Sign in Anonymously
-            Button (action: {
-                Task {
-                    do {
-                        // try to sign in Anonymously
-                        try await viewModel.signInAnonymous()
-                        // turn off the SignInView
-                        showSignInView = false;
-                    } catch {
-                        print(error)
-                    }
-                }
-            }, label: {
-                Text("Sign In Anonymously")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(height: 55)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.orange)
-                    .cornerRadius(10)
-            })
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
             
-            //            //  Sign in with Email
-            //            NavigationLink {
-            //                SignInEmailView(showSignInView: $showSignInView)
-            //            } label: {
-            //                Text("Sign In with Email")
-            //                    .font(.headline)
-            //                    .foregroundColor(.white)
-            //                    .frame(height: 55)
-            //                    .frame(maxWidth: .infinity)
-            //                    .background(Color.blue)
-            //                    .cornerRadius(10)
-            //            }
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundColor(Color.accentColor)
+                .rotationEffect(Angle(degrees: -20))
+                .frame(width: UIScreen.main.bounds.width * 3,
+                       height: 600)
+                .offset(y: 250)
             
-            //  Sign in with Google
-            GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .light, style: .wide, state: .normal)) {
-                Task {
-                    do {
-                        // try to sign in using Google
-                        let signedIn = try await viewModel.signInGoogle()
-                        if signedIn == true {
-                            // turn off the SignInView
-                            showSignInView = false
-                        }
-                    } catch let error as NSError {
-                        print(error.localizedDescription)
-                        if error.code == 401 {
-                            showNonAuthenticatedEmailAlert = true
-                        } else if error.code == 400 {
-                            showInvalidEmailAlert = true
-                        } else {
-                            
-                            //dont turn off signInView if user cant login(doesnt have authenticated email)
-                            //show alert
-                            showInvalidEmailAlert = true
-                        }
-                    } catch {
-                        print(error)
-                    }
-                }
+            Image("DoITBinaryLogo")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 300)
+                .offset(y: -240)
+                .offset(x: -30)
+            
+            VStack {
+                Spacer()
+                
+                // Sign in with Google
+                googleSignInButton
+                    .padding(.horizontal)
+                    .padding(.bottom, 180)
             }
         }
-        
-        //            //  Sign in with Apple
-        //            Button(action: {
-        //                Task {
-        //                    do {
-        //                        // try to sign in using Apple
-        //                        try await viewModel.signInApple()
-        //                        // turn off the SignInView
-        //                        showSignInView = false;
-        //                    } catch {
-        //                        print(error)
-        //                    }
-        //                }
-        //            }, label: {
-        //                SignInWithAppleButtonViewRepresentable(type: .default, style: .black)
-        //                    .allowsHitTesting(false) // do not let user click it
-        //            })
-        //            .frame(height: 55)
-        
-        Spacer()
-        .padding()
-            .navigationTitle("Sign In")
-            .alert("Invalid Email", isPresented: $showInvalidEmailAlert) {
-                Button("OK") { }
-            } message: {
-                Text("Email must end in umsystem.edu")
+        .alert("Invalid Email", isPresented: $showInvalidEmailAlert) {
+            Button("OK") { }
+        } message: {
+            Text("Email must end in umsystem.edu")
+        }
+        .alert("Email Not Authenticated", isPresented: $showNonAuthenticatedEmailAlert) {
+            Button("OK") { }
+        } message: {
+            Text("Your email is valid but not yet authenticated. Please contact support.")
+        }
+        .alert("No Email", isPresented: $showInvalidLoginAlert) {
+            Button("OK") { }
+        } message: {
+            Text("No Email Detected")
+        }
+    }
+    
+    private var anonymousSignInButton: some View {
+        // Sign In Anonymously
+        Button (action: {
+            Task {
+                do {
+                    // try to sign in Anonymously
+                    try await viewModel.signInAnonymous()
+                    // turn off the SignInView
+                    showSignInView = false;
+                } catch {
+                    print(error)
+                }
             }
-            .alert("Email Not Authenticated", isPresented: $showNonAuthenticatedEmailAlert) {
-                Button("OK") { }
-            } message: {
-                Text("Your email is valid but not yet authenticated. Please contact support.")
+        }, label: {
+            Text("Sign In Anonymously")
+                .font(.headline)
+                .foregroundColor(.white)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.orange)
+                .cornerRadius(10)
+        })
+    }
+    
+    private var emailSignInButton: some View {
+        //  Sign in with Email
+        NavigationLink {
+            SignInEmailView(showSignInView: $showSignInView)
+        } label: {
+            Text("Sign In with Email")
+                .font(.headline)
+                .foregroundColor(.white)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(10)
+        }
+    }
+    
+    private var googleSignInButton: some View {
+        //  Sign in with Google
+        Button {
+            Task {
+                do {
+                    // try to sign in using Google
+                    let signedIn = try await viewModel.signInGoogle()
+                    if signedIn == true {
+                        // turn off the SignInView
+                        showSignInView = false
+                    }
+                } catch let error as NSError {
+                    print(error.localizedDescription)
+                    if error.code == 401 {
+                        showNonAuthenticatedEmailAlert = true
+                    } else if error.code == 400 {
+                        showInvalidEmailAlert = true
+                    } else {
+                        
+                        //dont turn off signInView if user cant login(doesnt have authenticated email)
+                        //show alert
+                        showInvalidEmailAlert = true
+                    }
+                } catch {
+                    print(error)
+                }
             }
-            .alert("No Email", isPresented: $showInvalidLoginAlert) {
-                Button("OK") { }
-            } message: {
-                Text("No Email Detected")
+        } label: {
+            HStack {
+                Image("GoogleIcon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 30)
+                Text("Sign in with Google")
+                    .font(.headline)
+                    .foregroundColor(Color.gray)
             }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            
+        }
+        .background(Color.white)
+        .cornerRadius(10)
+        .frame(width: 340)
+    }
+    
+    private var appleSignInButton: some View {
+        //  Sign in with Apple
+        Button(action: {
+            Task {
+                do {
+                    // try to sign in using Apple
+                    try await viewModel.signInApple()
+                    // turn off the SignInView
+                    showSignInView = false;
+                } catch {
+                    print(error)
+                }
+            }
+        }, label: {
+            SignInWithAppleButtonViewRepresentable(type: .default, style: .black)
+                .allowsHitTesting(false) // do not let user click it
+        })
+        .frame(height: 55)
     }
 }
 
